917eae413ca383e3d7c39525fccda5006b6b68d9
Alejandro Rueda <arueda@boldrimpact.com>
2023-10-03 12:41:55 -0600

    KT-1 Config 3

---
+++ app/Http/Controllers/AsignacionesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AsignacionesController extends Controller
+{
+    public function asignar($id)
+    {
+        dd($id);
+    }
+}
